dadaa2ade11b59aa2afb417b41382d69269f0097
ledtudev <led.tu100@gmail.com>
2025-12-25 23:55:52 +0700

    fixbug

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,33 @@
-import Demo1Page from "./demo1/page";
+import { BlogGrid } from "./_components/BlogGrid";
+import { HeroSection } from "./_components/HeroSection";
+import { InfoSection } from "./_components/InfoSection";
+import { NewsletterBanner } from "./_components/NewsletterBanner";
+import { PortfolioGrid } from "./_components/PortfolioGrid";
+import { PricingSection } from "./_components/PricingSection";
+import { ProductGrid } from "./_components/ProductGrid";
+import { PromoBanner } from "./_components/PromoBanner";
+import { ServiceGrid } from "./_components/ServiceGrid";
+import { SiteFooter } from "./_components/SiteFooter";
+import { SiteHeader } from "./_components/SiteHeader";
+import { TestimonialSlider } from "./_components/TestimonialSlider";
 
-export default function Page() {
-  return <Demo1Page />;
+export default function Demo1Page() {
+  return (
+    <div className="min-h-screen bg-white font-sans antialiased text-etchy-black selection:bg-etchy-yellow selection:text-black">
+      <SiteHeader />
+      <main className="flex flex-col">
+        <HeroSection />
+        <ServiceGrid />
+        <PortfolioGrid />
+        <InfoSection />
+        <PricingSection />
+        <ProductGrid />
+        <PromoBanner />
+        <TestimonialSlider />
+        <BlogGrid />
+        <NewsletterBanner />
+      </main>
+      <SiteFooter />
+    </div>
+  );
 }
